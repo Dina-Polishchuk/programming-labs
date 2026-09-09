fe92c7f41c85c87d69ceb09e23fe697c679fec05
Dins-Polishchuk <diana.qline@icloud.com>
2026-09-09 17:10:09 +0300

    First comint: Add laba.1.py

---
+++ laba.1.py
@@ -0,0 +1,31 @@
+import numbers
+
+print("Hello World")
+name = 'Шнек'
+age = 52
+height = 1.67
+student = True
+sabjects = ["Math", "English", "History"]
+Bobik = (10,20)
+student_info = {
+    "name": "Шнек",
+    "age": 52,
+    "height": 1.67,
+}
+number = {1,2,3,4}
+print(name,type(name))
+print(age,type(age))
+print(height,type(height))
+print(student,type(student))
+print(sabjects,type(sabjects))
+print(Bobik,type(Bobik))
+print(student_info,type(student_info))
+print(number,type(number))
+print(22+12)
+print(22-12)
+print(22/12)
+print(22//12)
+print(22%12)
+print(22**12)
+print(22*12)
+print(22==12)
